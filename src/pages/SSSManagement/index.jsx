504aa918264b62412685a7933453802c70b12fb9
eminakcil <eminakcil@outlook.com>
2023-07-18 14:38:03 +0300

    faqs

--- src/pages/SSSManagement/index.jsx
+++ src/pages/SSSManagement/index.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react"
-import { FAQService } from "../../services"
-import { Loading } from "../../components/LoadingSpinner"
-import { Card } from "flowbite-react"
-import ScrollToTop from "../../components/ScrollToTop"
+import { useEffect, useState } from 'react'
+import { FAQService } from '../../services'
+import { Loading } from '../../components/LoadingSpinner'
+import { Card } from 'flowbite-react'
+import ScrollToTop from '../../components/ScrollToTop'
 
 const index = () => {
   const [loading, setLoading] = useState(true)
@@ -26,27 +26,26 @@ const index = () => {
             <div className="text-center text-3xl font-extrabold text-gray-400">
               Hakkımızda Merak Edilenler
             </div>
-            <section class="bg-white w-4/5 px-10 pb-8 pt-10 rounded-lg flex justify-between flex-row">
-              <div class="w-2/6">
-                <p class="text-xl font-semibold pr-5"></p>
+            <section className="bg-white w-4/5 px-10 pb-8 pt-10 rounded-lg flex justify-between flex-row">
+              <div className="w-2/6">
+                <p className="text-xl font-semibold pr-5"></p>
               </div>
-              <div class="w-3/4 space-y-2">
-                {items.map((item, id) => (
+              <div className="w-3/4 space-y-2">
+                {items.map((item) => (
                   <details
                     key={item.id}
-                    class="border border-slate-200 rounded pt-2 pb-3 px-3 relative open:shadow-lg mb-1 bg-slate-200 open:bg-white duration-300"
+                    className="border border-slate-200 rounded pt-2 pb-3 px-3 relative open:shadow-lg mb-1 bg-slate-200 open:bg-white duration-300"
                   >
-                    <summary class="list-none font-semibold relative text-sm cursor-pointer pr-7">
+                    <summary className="list-none font-semibold relative text-sm cursor-pointer pr-7">
                       {item?.question}
-                      <div class="absolute top-0 right-0 bg-slate-200 rounded-full px-1 py-0.5 cursor-pointer visible open:invisible">
+                      <div className="absolute top-0 right-0 bg-slate-200 rounded-full px-1 py-0.5 cursor-pointer visible open:invisible">
                         <svg
-                          class="h-5 w-4 text-pink-700"
+                          className="h-5 w-4 text-pink-700"
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
                           strokeWidth="{1.5}"
                           stroke="currentColor"
-                          className="w-6 h-6"
                         >
                           <path
                             strokeLinecap="round"
@@ -56,8 +55,8 @@ const index = () => {
                         </svg>
                       </div>
                     </summary>
-                    <p class="text-xs pt-3">
-                      <p>{item?.answer}</p>
+                    <p className="text-xs pt-3">
+                      <p dangerouslySetInnerHTML={{ __html: item?.answer }}></p>
                     </p>
                   </details>
                 ))}
