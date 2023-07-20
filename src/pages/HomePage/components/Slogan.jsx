f0b51cc03757ceed28626698d164550a56755b37
essraay <esra-1-@hotmail.com>
2023-07-20 15:50:04 +0300

    .

--- src/pages/HomePage/components/Slogan.jsx
+++ src/pages/HomePage/components/Slogan.jsx
@@ -58,12 +58,20 @@ const Slogan = () => {
             />
           </div>
         </div>
-        <div className="lg:mt-0 lg:col-span-5 lg:flex">
+        <div className="lg:mt-0 lg:col-span-5 lg:flex items-center my-auto">
+          <div className="space-y-4">
+            <span className="flex w-2.5 h-2.5 bg-gray-700 rounded-full mr-1.5 flex-shrink-0"></span>
+            <span className="flex w-2.5 h-2.5 bg-gray-600 rounded-full mr-1.5 flex-shrink-0"></span>
+            <span className="flex w-2.5 h-2.5 bg-gray-500 rounded-full mr-1.5 flex-shrink-0"></span>
+            <span className="flex w-2.5 h-2.5 bg-gray-400 rounded-full mr-1.5 flex-shrink-0"></span>
+            <span className="flex w-2.5 h-2.5 bg-gray-300 rounded-full mr-1.5 flex-shrink-0"></span>
+            <span className="flex w-2.5 h-2.5 bg-gray-200 rounded-full mr-1.5 flex-shrink-0"></span>
+          </div>
           <SliderBnA
             before="/afterbefore/Sketch5.png"
             after="/afterbefore/Sketch6.png"
             divisorBorder={true}
-            imageHeight={80}
+            imageHeight={66}
           />
         </div>
         <div className="p-5"></div>
